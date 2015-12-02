c7d3f53d7e6369f94b0cd521b16ce1cb5de7d868
dujie <760813193@qq.com>
2015-12-02 22:14:50 +0800

    only-dev-server change to dev-server

--- webpack-dev-server.js
+++ webpack-dev-server.js
@@ -1,14 +1,22 @@
 var webpack = require('webpack');
 var webpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config');
+var host = 'localhost';
 var port = 8080;
 
-webpackConfig.entry.unshift('webpack-dev-server/client?http://localhost:8080','webpack/hot/dev-server');
+webpackConfig.entry.unshift('webpack-dev-server/client?http://' + host + ':' + port, 'webpack/hot/dev-server');
 var compiler = webpack(webpackConfig);
 var server = new webpackDevServer(compiler, {
-	hot:true,
-	noInfo: true,
-	publicPath: webpackConfig.output.publicPath
+    hot: true,
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    }
+}).listen(port, host, function() {
+    console.info("==> 🌍 Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
 });
-server.listen(port);
-console.info("==> 🌍 Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
