0d3ee26155988ebbcd6d447f5a3d9a11970d73c5
dujie <760813193@qq.com>
2015-12-05 15:15:27 +0800

    update

--- settings.js
+++ settings.js
@@ -4,16 +4,14 @@ var mode = process.argv[process.argv.length - 1];
 var settings = {
     __DEV__: false,
     WDS_PORT: 3000,
-    BASE_URL: JSON.stringify("http://127.0.0.1:8080")
+    // BASE_URL: JSON.stringify("http://192.168.0.179:8080"),
+    BASE_URL: JSON.stringify("http://123.57.226.128:8080")
+
 };
 if (mode == "dev") {
     Object.assign(settings, {
         __DEV__: true,
-        BASE_URL: JSON.stringify("http://192.168.0.179:8080")
-    });
-} else if (mode == "pro") {
-    Object.assign(settings, {
-        BASE_URL: JSON.stringify("http://123.57.226.128:8080")
+        BASE_URL: JSON.stringify("http://localhost:8080")
     });
 }
 
